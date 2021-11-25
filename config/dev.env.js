8cb01ea722ac91b908aa9714a8023c97a59f84af
Ezra Lazuardy <ezralucio@gmail.com>
2021-11-25 15:12:30 +0700

    Update

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,10 @@
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
 const keys = require('../keys')
 
-module.exports = {
+module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  GOOGLE_API_KEY: keys.dev,
+  GOOGLE_API_KEY: keys.GOOGLE_API_KEY,
   GEOCODE_API_URL: '"http://localhost:3000/geocode/v1/json?"',
   WEATHER_API_URL: '"http://localhost:3000/weather/v1/json?"'
-}
+})
