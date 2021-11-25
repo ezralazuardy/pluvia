e22cc95ff6e58dd98cd6dc4f888f251d70c1d5ba
Ezra Lazuardy <ezralucio@gmail.com>
2021-11-25 15:36:11 +0700

    Update

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,12 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  GOOGLE_API_KEY: '""',
-  GEOCODE_API_URL: '""',
-  WEATHER_API_URL: '""'
+if (process.env.VERCEL_ENV === 'production') {
+  module.exports = {
+    NODE_ENV: '"production"'
+  }
+} else {
+  module.exports = {
+    NODE_ENV: '"production"',
+    GOOGLE_API_KEY: '""',
+    GEOCODE_API_URL: '""',
+    WEATHER_API_URL: '""'
+  }
 }
