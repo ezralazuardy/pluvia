7538b4be8a6a8280221500ded959eded08a432c9
Ezra Lazuardy <ezralucio@gmail.com>
2021-11-25 15:42:39 +0700

    Update

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,12 +1,3 @@
-if (process.env.VERCEL_ENV === 'production') {
-  module.exports = {
-    NODE_ENV: '"production"'
-  }
-} else {
-  module.exports = {
-    NODE_ENV: '"production"',
-    GOOGLE_API_KEY: '""',
-    GEOCODE_API_URL: '""',
-    WEATHER_API_URL: '""'
-  }
+module.exports = {
+  NODE_ENV: '"production"'
 }
